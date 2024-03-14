87aa69102edb06228d1abd35f78ec8cb57ea0fc8
Cstan <tancs8803@gmail.com>
2024-03-14 18:57:00 +0800

    Set Up 1.4

--- Process/search_Process.php
+++ Process/search_Process.php
@@ -1,16 +1,9 @@
 <?php
-
-<<<<<<< Updated upstream
-include("config.php");
-if($_POST["RTSearch_verify"] && !empty($_POST["RealTimeSearch"])) {
-    $RTSearch = $_POST["RealTimeSearch"];
-    $sql = "SELECT * FROM Movie_Event_Table WHERE Event_keyword_name LIKE %$RTSearch% OR Event_name='$RTSearch'";
-=======
 include("../config.php");
 if($_POST["RTSearch_verify"] && !empty($_POST["RealTimeSearch"])) {
     $RTSearch = strtolower($_POST["RealTimeSearch"]);
     $sql = "SELECT  LOWER(*) FROM Movie_Event_Table WHERE Event_keyword_name LIKE %$RTSearch% OR Event_name='$RTSearch'";
->>>>>>> Stashed changes
+
     $result = $conn->query($sql);
 
     while($row = $result->fetch_assoc()) {
@@ -20,13 +13,13 @@ if($_POST["RTSearch_verify"] && !empty($_POST["RealTimeSearch"])) {
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     if(!empty(trim($_POST["search"]))) {
-<<<<<<< Updated upstream
+
         $search = $_POST["search"];
         $sql = "SELECT Event_name FROM Movie_Event_Table WHERE Event_name = $search";
-=======
+
         $search = strtolower($_POST["search"]);
         $sql = "SELECT LOWER(*) FROM Movie_Event_Table WHERE Event_name = $search";
->>>>>>> Stashed changes
+
         $result = $conn->query($sql);
         if($result->num_rows <= 0) {
             die("Row Not found");
