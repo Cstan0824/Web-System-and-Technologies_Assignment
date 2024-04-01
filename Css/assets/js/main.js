fee6423b51d665fe36a72441968c2966adbe9717
Shaohang16410 <harrisontsh-wm23@student.tarc.edu.my>
2024-04-01 13:54:48 +0800

    test

--- Css/assets/js/main.js
+++ Css/assets/js/main.js
@@ -260,3 +260,11 @@
   new PureCounter();
 
 })()
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
