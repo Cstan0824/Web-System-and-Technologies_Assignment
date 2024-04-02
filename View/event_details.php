d38cbc4cf5951702f2e74dc1bac1dccdfccd32e5
Cstan <tancs8803@gmail.com>
2024-04-02 13:33:54 +0800

    Event_details page edit

--- View/event_details.php
+++ View/event_details.php
@@ -9,54 +9,12 @@
 	<meta content="" name="description">
 	<meta content="" name="keywords">
 
-	<!-- Favicons -->
-	<link href="../Css/assets/img/favicon.png" rel="icon">
-	<link href="../Css/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-	<!-- Google Fonts -->
-	<link
-		href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-		rel="stylesheet">
-
-	<!-- Vendor CSS Files -->
-	<link href="../Css/assets/vendor/aos/aos.css" rel="stylesheet">
-	<link href="../Css/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link href="../Css/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-	<link href="../Css/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-	<link href="../Css/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-	<link href="../Css/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-	<!-- Template Main CSS File -->
-	<link href="../Css/assets/css/style.css" rel="stylesheet">
+	<?php include("../Root/link.php") ?>
 
 </head>
 
 <body>
-
-	<!-- ======= Header ======= -->
-	<header id="header" class="fixed-top ">
-		<div class="container d-flex align-items-center justify-content-between position-relative">
-
-			<div class="logo">
-				<h1 class="text-light"><a href="Home.php"><span>TARUMT Movie Society</span></a></h1>
-				<!-- Uncomment below if you prefer to use an image logo -->
-				<!-- <a href="index.html"><img src="../Css/assets/img/logo.png" alt="" class="img-fluid"></a>-->
-			</div>
-
-			<nav id="navbar" class="navbar">
-				<ul>
-					<li><a class="nav-link scrollto" href="#about">Activities</a></li>
-					<li><a class="nav-link scrollto" href="#services">Ticket</a></li>
-					<li><a class="nav-link scrollto" href="#portfolio">Join Us</a></li>
-					<li><a class="nav-link scrollto" href="#team">About Us</a></li>
-					<li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-				</ul>
-				<i class="bi bi-list mobile-nav-toggle"></i>
-			</nav><!-- .navbar -->
-
-		</div>
-	</header><!-- End Header -->
-
+	<?php include("header.php") ?>
 	<main id="main">
 
 		<!-- ======= Breadcrumbs Section ======= -->
@@ -84,7 +42,7 @@
 								<div class="swiper-wrapper align-items-center">
 
 									<div class="swiper-slide">
-										<img src="../Css/assets/img/portfolio/test1.png" alt="">
+										<img class="rounded" src="../Css/assets/img/portfolio/test1.png" alt="">
 									</div>
 
 								</div>
@@ -93,11 +51,11 @@
 						</div>
 
 						<div class="col-lg-4">
-							<div class="portfolio-info">
+							<div class="portfolio-info rounded-sm">
 								<h3>Movie information</h3>
 								<ul>
 									<li><strong>Movie Name</strong>: Scent OF A Woman</li>
-									<li><strong>Starring</strong>: Al Pacino，Chris O'Donnell</li>
+									<li><strong>Starring</strong>: Al Pacino, Chris O'Donnell</li>
 									<li><strong>Release date</strong>: December 23, 1992</li>
 									<li><strong>Language</strong>: English</li>
 									<li><strong>Location</strong>: West Campus . DK4</li>
@@ -128,26 +86,15 @@
 	<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
 			class="bi bi-arrow-up-short"></i></a>
 
-	<!-- Vendor JS Files -->
-	<script src="../Css/assets/vendor/purecounter/purecounter_vanilla.js"></script>
-	<script src="../Css/assets/vendor/aos/aos.js"></script>
-	<script src="../Css/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<script src="../Css/assets/vendor/glightbox/js/glightbox.min.js"></script>
-	<script src="../Css/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-	<script src="../Css/assets/vendor/swiper/swiper-bundle.min.js"></script>
-
-
 	<!-- Template Main JS File -->
 	<script src="../Css/assets/js/main.js"></script>
 	<script>
 		function myFunction() {
 			var txt;
 			if (confirm("You have successfully booked the ticket.")) {
-				txt = "You pressed OK!";
-			} else {
-				txt = "You pressed Cancel!";
+				//eprint("You pressed OK!");
+				window.location.href = "Home.php";
 			}
-			document.getElementById("demo").innerHTML = txt;
 		}
 	</script>
 
