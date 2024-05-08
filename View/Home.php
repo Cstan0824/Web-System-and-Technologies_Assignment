889143897b801b9f2c44ffe49783e4cca1d07dd3
Shaohang16410 <harrisontsh-wm23@student.tarc.edu.my>
2024-05-08 15:50:15 +0800

    .

--- View/Home.php
+++ View/Home.php
@@ -94,8 +94,7 @@
 						<div class="portfolio-wrap">
 							<img src="../Css/assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
 							<div class="portfolio-links">
-								<a href="../Css/assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery"
-									class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
+								<a href="../Css/assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery"></a>
 								<a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
 							</div>
 						</div>
