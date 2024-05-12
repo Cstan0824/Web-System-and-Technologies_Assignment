a2b1241841ce98dc6da47c5cec52f2c2ab1076d8
Cstan <tancs8803@gmail.com>
2024-05-12 15:05:03 +0800

    event_details UI improvement ver2.0

--- View/event_details.php
+++ View/event_details.php
@@ -162,7 +162,7 @@
 			<section id="booking-details" class="portfolio-details">
 				<div class="container portfolio-info">
 					<div class="section-title">
-						<h2>Booking Details</h2>
+						<h2>Member Details</h2>
 					</div>
 					<div class="table-responsive" data-aos="fade-up">
 						<?php
@@ -221,7 +221,7 @@
 			        }
 			        echo "</form></tbody></table>";
 			    }
-			}?>
+			    ?>
 
 					</div>
 					<form
@@ -255,7 +255,7 @@
 						</ul>
 					</form>
 				</div>
-
+				<?php } ?>
 			</section>
 			<!-- ======= End Booking Details Section ======= -->
 
